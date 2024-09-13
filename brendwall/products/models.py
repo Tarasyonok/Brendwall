11001aa2fd56b0cc69728451e9048cd935ba3a0c
Kirill Tarasov <kir_tarasov@mail.ru>
2024-09-13 12:25:32 +0300

    feat: Create product model

--- brendwall/products/models.py
+++ brendwall/products/models.py
@@ -1,3 +1,12 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    id = models.BigAutoField("id", primary_key=True)
+    name = models.CharField("Название", max_length=255, null=False, blank=False)
+    description = models.TextField("Описание", null=True, blank=True)
+    price = models.IntegerField("Цена", validators=[MinValueValidator(1)], null=False, blank=False)
+
+    class Meta:
+        db_table = "products"
