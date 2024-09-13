b28c45d0269f84a2dbf9182ff6ba679c6f7c886b
Kirill Tarasov <kir_tarasov@mail.ru>
2024-09-13 13:52:06 +0300

    feat: Add product with form

--- brendwall/static/js/products/add_product.js
+++ brendwall/static/js/products/add_product.js
@@ -1,3 +1,4 @@
+let formNode = document.querySelector("#product-form");
 let tableContentNode = document.querySelector("#table-content");
 
 
@@ -5,7 +6,6 @@ function loadProductsTable() {
     try {
         fetch("/api").then((response) => {
             response.json().then((products) => {
-                console.log(products);
                 tableContentNode.innerHTML = '';
                 for (let product of products) {
                     tableContentNode.innerHTML += `
@@ -18,11 +18,63 @@ function loadProductsTable() {
                 }
             });
         });
-    } catch(error) {
+    } catch (error) {
         console.error(error);
     }
 }
 
+
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
+
+function addProduct(data) {
+    try {
+        let csrftoken = getCookie('csrftoken');
+        let response = fetch('/api/', {
+            method: 'POST',
+            headers: {
+                'X-CSRFToken': csrftoken
+            },
+            body: data,
+        }).then((response) => response.json()
+            .then((data) => ({ status: response.status, body: data })))
+            .then((result) => {
+                if (result.status == 200) {
+                    tableContentNode.innerHTML = `
+                      <tr>
+                        <td>${data.get("name")}</td>
+                        <td>${data.get("description")}</td>
+                        <td>${data.get("price")}</td>
+                      </tr>
+                ` + tableContentNode.innerHTML;
+                }
+            });
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+
+formNode.addEventListener('submit', (event) => {
+    event.preventDefault();
+    let data = new FormData(formNode);
+    addProduct(data);
+    formNode.reset();
+});
 
 loadProductsTable();
 
