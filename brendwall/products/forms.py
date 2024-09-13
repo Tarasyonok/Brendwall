9dfcce400097ac426cc169217bb9b3b78c77d6ab
Kirill Tarasov <kir_tarasov@mail.ru>
2024-09-13 12:58:03 +0300

    feat: Add product endpoint

--- brendwall/products/forms.py
+++ brendwall/products/forms.py
@@ -6,4 +6,4 @@ from products.models import Product
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
-        fields = ['name', 'description', 'price']
+        fields = ["name", "description", "price"]
--- brendwall/products/views.py
+++ brendwall/products/views.py
@@ -1,7 +1,17 @@
 from django.http import JsonResponse
 
 from products.models import Product
+from products.forms import ProductForm
 
 
 def products_api(request):
-    return JsonResponse(list(Product.objects.all().values()), safe=False)
+    if request.method == "POST":
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return JsonResponse({"message": f'Продукт "{request.POST["name"]}" добавлен'}, status=200)
+        else:
+            return JsonResponse({"message": "Форма заполнена некорректно"}, status=400)
+
+    else:
+        return JsonResponse(list(Product.objects.all().values()), safe=False)
